2ab95c7f7e78e698db614cc7ec0f1d155aebaea2
Victor George <vic@topcoder.com>
2018-06-14 19:26:24 -0700

    Security vulnerabilities fixed via "npm audit autofix", added storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/react';
+import 'react-chromatic/storybook-addon';
+
+// 2. If you have a global CSS file, import it here:
+// import '../src/styles/main.css';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
