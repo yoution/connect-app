5bb60b2aa52314361589e444dd9de0ba1050840d
RishiRaj <rishiraj.sahu@topcoder.com>
2018-12-28 12:46:50 +0530

    Added support for `message` type subsection

--- src/projects/detail/components/SpecSection.jsx
+++ src/projects/detail/components/SpecSection.jsx
@@ -201,6 +201,16 @@ const SpecSection = props => {
           />
         </div>
       )
+    case 'message':
+    return (
+      <div>
+        {!isReadOnly && (
+          <div className="message-title">
+            {props.description}
+          </div>
+        )}
+      </div>
+    )
     case 'files': {
       const projectLatest = isProjectDirty ? dirtyProject : project
       const files = _.get(projectLatest, props.fieldName, [])
